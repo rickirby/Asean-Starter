8f59ac9722e0d116783747040b881c3108930920
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-07-15 07:43:37 +0700

    Initial Commit

---
+++ Asean/Country.swift
@@ -0,0 +1,22 @@
+//
+//  Country.swift
+//  Asean
+//
+//  Created by Ricki Bin Yamin on 15/07/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+struct Country {
+    var country: String
+    var capital: String
+    var independence: String
+    var photos: [UIImage?]
+    var flag: UIImage?
+    var semboyan: String
+    var nationalAnthem: String
+    var languages: String
+    var language: String
+    var information: String
+}
